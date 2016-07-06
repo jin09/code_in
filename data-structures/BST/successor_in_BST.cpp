bcc8255484872753b19c4b13978b89bbf34ed736
jin09 <supergautam995@gmail.com>
2016-07-06 07:29:25 +0530

    case 1 and case 2

---
+++ data-structures/BST/successor_in_BST.cpp
@@ -0,0 +1,199 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class BST;
+
+class binaryTreeNode{
+public:
+    int data;
+    binaryTreeNode* left;
+    binaryTreeNode* right;
+    binaryTreeNode* parent;
+
+    binaryTreeNode(int data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+        parent = NULL;
+    }
+};
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   PRETTY PRINT   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+
+// Find the maximum height of the binary tree
+int maxHeight(binaryTreeNode *p) {
+  if (!p) return 0;
+  int leftHeight = maxHeight(p->left);
+  int rightHeight = maxHeight(p->right);
+  return (leftHeight > rightHeight) ? leftHeight + 1: rightHeight + 1;
+}
+
+// Convert an integer value to string
+string intToString(int val) {
+  ostringstream ss;
+  ss << val;
+  return ss.str();
+}
+
+// Print the arm branches (eg, /    \ ) on a line
+void printBranches(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<binaryTreeNode*>& nodesQueue, ostream& out) {
+  deque<binaryTreeNode*>::const_iterator iter = nodesQueue.begin();
+  for (int i = 0; i < nodesInThisLevel / 2; i++) {
+    out << ((i == 0) ? setw(startLen-1) : setw(nodeSpaceLen-2)) << "" << ((*iter++) ? "/" : " ");
+    out << setw(2*branchLen+2) << "" << ((*iter++) ? "\\" : " ");
+  }
+  out << endl;
+}
+
+// Print the branches and node (eg, ___10___ )
+void printNodes(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<binaryTreeNode*>& nodesQueue, ostream& out) {
+  deque<binaryTreeNode*>::const_iterator iter = nodesQueue.begin();
+  for (int i = 0; i < nodesInThisLevel; i++, iter++) {
+    out << ((i == 0) ? setw(startLen) : setw(nodeSpaceLen)) << "" << ((*iter && (*iter)->left) ? setfill('_') : setfill(' '));
+    out << setw(branchLen+2) << ((*iter) ? intToString((*iter)->data) : "");
+    out << ((*iter && (*iter)->right) ? setfill('_') : setfill(' ')) << setw(branchLen) << "" << setfill(' ');
+  }
+  out << endl;
+}
+
+// Print the leaves only (just for the bottom row)
+void printLeaves(int indentSpace, int level, int nodesInThisLevel, const deque<binaryTreeNode*>& nodesQueue, ostream& out) {
+  deque<binaryTreeNode*>::const_iterator iter = nodesQueue.begin();
+  for (int i = 0; i < nodesInThisLevel; i++, iter++) {
+    out << ((i == 0) ? setw(indentSpace+2) : setw(2*level+2)) << ((*iter) ? intToString((*iter)->data) : "");
+  }
+  out << endl;
+}
+
+// Pretty formatting of a binary tree to the output stream
+// @ param
+// level  Control how wide you want the tree to sparse (eg, level 1 has the minimum space between nodes, while level 2 has a larger space between nodes)
+// indentSpace  Change this to add some indent space to the left (eg, indentSpace of 0 means the lowest level of the left node will stick to the left margin)
+void printPretty(binaryTreeNode *root, int level, int indentSpace, ostream& out) {
+  int h = maxHeight(root);
+  int nodesInThisLevel = 1;
+
+  int branchLen = 2*((int)pow(2.0,h)-1) - (3-level)*(int)pow(2.0,h-1);  // eq of the length of branch for each node of each level
+  int nodeSpaceLen = 2 + (level+1)*(int)pow(2.0,h);  // distance between left neighbor node's right arm and right neighbor node's left arm
+  int startLen = branchLen + (3-level) + indentSpace;  // starting space to the first node to print of each level (for the left most node of each level only)
+
+  deque<binaryTreeNode*> nodesQueue;
+  nodesQueue.push_back(root);
+  for (int r = 1; r < h; r++) {
+    printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+    branchLen = branchLen/2 - 1;
+    nodeSpaceLen = nodeSpaceLen/2 + 1;
+    startLen = branchLen + (3-level) + indentSpace;
+    printNodes(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+
+    for (int i = 0; i < nodesInThisLevel; i++) {
+      binaryTreeNode *currNode = nodesQueue.front();
+      nodesQueue.pop_front();
+      if (currNode) {
+	      nodesQueue.push_back(currNode->left);
+	      nodesQueue.push_back(currNode->right);
+      } else {
+        nodesQueue.push_back(NULL);
+        nodesQueue.push_back(NULL);
+      }
+    }
+    nodesInThisLevel *= 2;
+  }
+  printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+  printLeaves(indentSpace, level, nodesInThisLevel, nodesQueue, out);
+}
+
+/*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  END OF PRETTY PRINT   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+
+int findMin(binaryTreeNode* root){
+    if(root == NULL){
+        return -1;
+    }
+    if(root->left == NULL){
+        return root->data;
+    }
+    else{
+        return findMin(root->left);
+    }
+}
+
+void insertIntoBST(binaryTreeNode*& root, int data){
+    if(root == NULL){
+        root = new binaryTreeNode(data);
+        root->left = NULL;
+        root->right = NULL;
+        return;
+    }
+    if(data < root->data){
+        insertIntoBST(root->left,data);
+    }
+    if(data > root->data){
+        insertIntoBST(root->right,data);
+    }
+}
+
+    int successorHelper(binaryTreeNode* root , int data , int case_1_element){
+        if(root == NULL){
+            return -1;
+        }
+        if(data < root->data){
+            return successorHelper(root->left,data, root->data);
+        }
+        else if(data > root->data){
+            return successorHelper(root->right, data, case_1_element);
+        }
+        else if(data == root->data){
+            //element is found
+            if(root->right == NULL){
+                //case 2
+                return case_1_element;
+            }
+            else{
+                return findMin(root->right);
+            }
+        }
+    }
+
+    int successor(binaryTreeNode* root, int data){
+        if(root == NULL){
+            return -1;
+        }
+        if(data < root->data){
+
+            return successorHelper(root->left,data,root->data);
+        }
+        if(data > root->data){
+            return successorHelper(root->right,data,-1);
+        }
+        else{
+            //rooot element is the data to be found
+            if(root->right == NULL){
+                //root element is the greatest
+                return -1;
+            }
+            else{
+                return findMin(root->right);
+            }
+        }
+    }
+
+int main(){
+
+binaryTreeNode* root = NULL;
+insertIntoBST(root,15);
+insertIntoBST(root,10);
+insertIntoBST(root,20);
+insertIntoBST(root,8);
+insertIntoBST(root,12);
+insertIntoBST(root,11);
+printPretty(root,3,4,cout);
+//case 1
+cout<<successor(root,10)<<endl;
+//case 2
+cout<<successor(root,8)<<endl;
+cout<<successor(root,12)<<endl;
+//boundary case
+cout<<successor(root,20)<<endl;
+return 0;
+}
+
