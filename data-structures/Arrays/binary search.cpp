29d92598317f79bf8c153b3018411a03cbbfff51
jin09 <supergautam995@gmail.com>
2016-07-20 07:20:46 +0530

    easy

---
+++ data-structures/Arrays/binary search.cpp
@@ -0,0 +1,36 @@
+/*
+Binary Search
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+bool binarySearch(int* arr, int startIndex, int endIndex, int key){
+
+    if(startIndex > endIndex){
+
+        return false;
+    }
+
+    int mid = (startIndex + endIndex)/2;
+    if(arr[mid] == key){
+        return true;
+    }
+    if(key < arr[mid]){
+        return binarySearch(arr,startIndex,mid-1,key);
+    }
+    else{
+        return binarySearch(arr,mid+1,endIndex,key);
+    }
+}
+
+int main(){
+int arr[] = {1,2,3,4,5,6,7,8,9};
+int key = 7;
+if(binarySearch(arr,0,8,key)){
+    cout<<"Key is Present !!";
+}
+else{
+    cout<<"Key is absent";
+}
+return 0;
+}
