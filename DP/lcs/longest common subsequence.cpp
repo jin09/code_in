1f8b6e9465bd5ad2cc9d52e255fc7f46cd5bdabc
jin09 <supergautam995@gmail.com>
2016-07-23 10:21:41 +0530

    recursion

--- DP/lcs/longest common subsequence.cpp
+++ DP/lcs/longest common subsequence.cpp
@@ -8,14 +8,14 @@ int longestCommonSubsequence(char* a, char* b, int l1, int l2){
     if(l1<0 || l2<0){
         return 0;
     }
-
+ 
     if(a[l1] == b[l2]){
         return longestCommonSubsequence(a,b,l1-1,l2-1) + 1;
     }
 
     if(a[l1] != b[l2]){
         return max(longestCommonSubsequence(a,b,l1-1,l2),longestCommonSubsequence(a,b,l1,l2-1));
-    }
+    } 
 }
 
 int main(){
