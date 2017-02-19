859ec99a0f1a5c0e773c214d3db99839676fc16a
jin09 <supergautam995@gmail.com>
2017-02-20 00:53:27 +0530

    correct implementstion of heaps

--- data-structures/heaps/MIN heap.cpp
+++ data-structures/heaps/MIN heap.cpp
@@ -1,85 +1,59 @@
-/*   Priority queue MIN HEAP
-     Build heap - O(n)
-*/
-
 #include<bits/stdc++.h>
 using namespace std;
 
-class priorityQueueMin{
-
+class minHeap{
 vector<int> arr;
-int index;
-int currentSize;
-
 private:
-    void heapify(int i){
+    void minHeapify(int pos){
+        int leftChild = 2*pos+1;
+        int rightChild = 2*pos+2;
 
-        int leftChild = 2*i + 1;
-        int rightChild = 2*i + 2;
-        //both the children are in range of the array
-        if(leftChild<index && rightChild<index){
-            //the parent node is sitting wrong
-            if(arr[i]>arr[leftChild] || arr[i]>arr[rightChild]){
-                //the minimum of the the two children should go up
-                if(arr[leftChild] < arr[rightChild]){
-                    swap(arr[i],arr[leftChild]);
-                    heapify(leftChild);
-                }
-                else{
-                    swap(arr[i],arr[rightChild]);
-                    heapify(rightChild);
-                }
-            }
-            //parent node is sitting right , and there is no need to heapify so return.
-            else{
+        //both are in range
+        if(leftChild < arr.size() && rightChild < arr.size()){
+            if(arr[pos] <= arr[leftChild] && arr[pos] <= arr[rightChild]){
                 return;
             }
-        }
-        //only left child is in range of the array
-        if(leftChild<index){
-            //the parent node is sitting wrong
-            if(arr[i]>arr[leftChild]){
-                swap(arr[i],arr[leftChild]);
-                heapify(leftChild);
+            if(arr[leftChild] < arr[rightChild]){
+                swap(arr[leftChild], arr[pos]);
+                minHeapify(leftChild);
             }
-            //the parent node is sitting right , there is no need to heapify further
             else{
+                swap(arr[rightChild], arr[pos]);
+                minHeapify(rightChild);
+            }
+        }
+        //only left child is in range
+        if(leftChild < arr.size()){
+            if(arr[pos] <= arr[leftChild]){
                 return;
             }
-        }
-        //only right child is in range of the array
-        if(rightChild<index){
-            //the parent node is sitting wrong
-            if(arr[i]>rightChild){
-                swap(arr[i],arr[rightChild]);
-                heapify(rightChild);
-            }
-            //the parent node is sitting right and there isno need for heapify.
             else{
+                swap(arr[leftChild], arr[pos]);
+                minHeapify(leftChild);
+            }
+        }
+        if(rightChild < arr.size()){
+            if(arr[pos] <= arr[rightChild]){
                 return;
             }
+            else{
+                swap(arr[rightChild], arr[pos]);
+                minHeapify(rightChild);
+            }
         }
-        //both the children are out of the range of the array, which means that we have reached the leaf node
         else{
-            return ;
+            return;
         }
     }
+
 public:
-    priorityQueueMin(){
-        index = 0;
-        currentSize = 0;
-    }
 
-    void buildHeapFromArray(int* brr, int n){
-        //copy elements to your vector
+    void buidHeap(int* arr, int n){
         for(int i=0;i<n;i++){
-            arr.push_back(brr[i]);
+            this->arr.push_back(arr[i]);
         }
-        currentSize = n;
-        index = n;
         for(int i=n/2;i>=0;i--){
-
-            heapify(i);
+            minHeapify(i);
         }
     }
 
@@ -88,15 +62,27 @@ public:
     }
 
     int deleteMin(){
-        swap(arr[0],arr[index-1]);
-        int returnValue = arr[index - 1];
+        int save = arr[0];
+        swap(arr[0], arr[arr.size()-1]);
         arr.pop_back();
-        index--;
-        currentSize--;
-        heapify(0);
-       // print();
-        //cout<<endl;
-        return returnValue;
+        minHeapify(0);
+        return save;
+    }
+
+    void insertInHeap(int data){
+        arr.push_back(data);
+        int pos_of_new_element = arr.size()-1;
+        int parent = (pos_of_new_element-1)/2;
+        while(parent >= 0){
+            if(arr[pos_of_new_element] < arr[parent]){
+                swap(arr[pos_of_new_element], arr[parent]);
+                pos_of_new_element = parent;
+                parent = (parent-1)/2;
+            }
+            else{
+                break;
+            }
+        }
     }
 
     void print(){
@@ -105,27 +91,27 @@ public:
         }
         cout<<endl;
     }
+
+    bool isEmpty(){
+        if(arr.size() == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 };
 
-
 int main(){
-
-priorityQueueMin pq;
 int arr[] = {1,9,7,4,2,6};
-
-pq.buildHeapFromArray(arr,6);
-cout<<"Printing the priority queue after building heap from the array: ";
-pq.print();
-
-//pq.print();
-//cout<<endl;
-
-
-cout<<"Heap sort : ";
-
-for(int i=0;i<6;i++){
-    arr[i] = pq.deleteMin();
-    cout<<arr[i]<<" ";
+minHeap m;
+m.buidHeap(arr, 6);
+m.print();
+m.insertInHeap(0);
+m.print();
+cout<<"Heap Sort : ";
+while(!m.isEmpty()){
+    cout<<m.deleteMin()<<" ";
 }
 return 0;
 }
