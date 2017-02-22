4df026a6179570d59cdcb05c69fb5bdbeaaf2fca
jin09 <supergautam995@gmail.com>
2017-02-22 18:20:42 +0530

    working!!

--- data-structures/graphs/bellman_ford.cpp
+++ data-structures/graphs/bellman_ford.cpp
@@ -1,68 +1,77 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-//BellmanFord algo for shortest path
 
+class Graph{
+vector<vector<pair<int, int> > > arr;
+int vertices;
+public:
+    Graph(int v){
+        this->vertices = v;
+        this->arr.resize(vertices);
+    }
 
-int main(){
-    int a,b,c,v,e,s,d;
-    printf("Enter the no of vertices:");
-    scanf("%d",&v);
-    vector <list<pair<int , int> > > adjlist(v+1);
-    printf("Enter the no of edges:");
-    scanf("%d",&e);
-for(int i=0;i<e;i++){
+    void addEdge(int v, int u, int w){
+        arr[v].push_back(make_pair(u,w));
+        arr[u].push_back(make_pair(v,w));
+    }
 
-    scanf("%d%d%d",&a,&b,&c);
-    adjlist[a].push_back(make_pair(b,c));                               //DIRECTED
-}
-
-//following commented code can be used to traverse
-//or to print the adjacency list.
-
-/*printf("\nThe Adjacency List-\n");
-    // Printing Adjacency List
-    for (int i = 1; i < adjlist.size(); i++) {
-        printf("adjlist[%d] ", i);
-
-        list< pair<int, int> >::iterator itr = adjlist[i].begin();
-
-        while (itr != adjlist[i].end()) {
-            printf(" -> %d(%d)", itr->first, itr->second);
-            itr++;
+    void bellmanford(int source){
+        int* dist = new int[vertices];
+        for(int i=0;i<vertices;i++){
+            dist[i] = INT_MAX;
         }
-        printf("\n");
-    }*/
-
-
-    //BellmanFord
-    int i,j;
-    list< pair<int, int> >::iterator itr;
-    printf("Enter the start vertex:");
-    scanf("%d",&s);
-    bool visited[v+1];
-    memset(visited,false,sizeof(visited));
-    visited[s]=true;
-    int brr[v+2];
-    memset(brr,INT_MAX,sizeof(brr));
-    brr[s]=0;
-    for(i=0;i<v-1;i++){
-
-        for(j=1;j<=v;j++){
-
-            if(visited[j]==true){
-
-                for(itr=adjlist[j].begin();itr!=adjlist[j].end();itr++){
-
-                    brr[itr->first]=min(brr[itr->first],(itr->second)+brr[j]);
-                    visited[itr->first]=true;
+        dist[source] = 0;
+        bool isDistGettingBetter = false;
+        for(int i=0;i<vertices;i++){
+                isDistGettingBetter = false;
+            for(int j=0;j<arr.size();j++){
+                if(dist[j] != -1){
+                    for(int k=0;k<arr[j].size();k++){
+                        pair<int, int> top = arr[j][k];
+                        int new_dist = dist[j] + top.second;
+                        if(new_dist < dist[top.first]){
+                            dist[top.first] = new_dist;
+                            isDistGettingBetter = true;
+                        }
+                    }
                 }
             }
+            if(isDistGettingBetter == false){
+                break;
+            }
         }
+        if(isDistGettingBetter == true){
+            cout<<"The graph has negative edge cycles, so shortest dist cannot be found!!"<<endl;
+        }
+        else{
+            //print the dist
+            cout<<"shortest distance from source "<<source<<" is : "<<endl;
+            for(int i=0;i<vertices;i++){
+                cout<<"distance to "<<i<<" is: "<<dist[i]<<endl;
+            }
+        }
+        delete [] dist;
     }
-    for(i=1;i<=v;i++){
+};
 
-        printf("%d ",brr[i]);
-    }
-
+int main(){
+    Graph g(9);
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(2, 5, 4);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(4, 5, 10);
+    g.addEdge(5, 6, 2);
+    g.addEdge(6, 7, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+    //test the -ve edge case
+    g.addEdge(7,8,-50);
+    g.bellmanford(0);
 return 0;
 }
