c6411fc182c6cc6f522fb446302ddd937f3a3348
jin09 <supergautam995@gmail.com>
2017-02-24 19:58:09 +0530

    Update recursion.cpp

--- DP/knapsack/recursion.cpp
+++ DP/knapsack/recursion.cpp
@@ -1,30 +1,23 @@
-/*
-Knapsack problem Recursion
-*/
 #include<bits/stdc++.h>
 using namespace std;
 
-int knapsackRecursive(int* wts,int* values,int capacity, int n ){
+int knapsack(int val[], int wt[], int capacity, int n){
     if(n<0){
         return 0;
     }
-
-    if(wts[n]>capacity){
-        return knapsackRecursive(wts,values,capacity,n-1);
+    if(capacity >= wt[n]){
+        return max(knapsack(val, wt, capacity-wt[n], n-1)+val[n],knapsack(val, wt, capacity, n-1));
     }
-
-    int includingCurrentItem = knapsackRecursive(wts,values,capacity-wts[n],n-1) + values[n];
-    int excludingCurrentItem = knapsackRecursive(wts,values,capacity,n-1);
-
-    return max(includingCurrentItem,excludingCurrentItem);
+    else{
+        return knapsack(val, wt, capacity, n-1);
+    }
 }
 
 int main(){
-int val[] = {60, 100, 120};
-int wt[] = {10, 20, 30};
-int  W = 50;
-int n = sizeof val/sizeof val[0];
-int result = knapsackRecursive(wt,val,W,n-1);
-cout<<"Total value that can be picked is : "<<result;
-return 0;
+    int val[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int  W = 50;
+    int n = sizeof(val)/sizeof(val[0]);
+    cout<<knapsack(val, wt, W, n-1);
+    return 0;
 }
