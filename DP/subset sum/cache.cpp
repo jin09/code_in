2727c37fd919418d83a87a4dd23660d10704ab11
jin09 <jin09@users.noreply.github.com>
2017-06-22 11:16:39 +0530

    last commit was way too complicated, this new method of mine is much simpler and cleaner

--- DP/subset sum/cache.cpp
+++ DP/subset sum/cache.cpp
@@ -1,44 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool subsetSumCache(int arr[], int sum, int n, int** cache){
+bool subsetSum(int arr[], int sum, int n){
     if(n < 0){
+        return false;
+    }
+    if(sum < 0){
         return false;
     }
     if(sum == 0){
         return true;
     }
+    if(arr[n] <= sum){
+        bool including = subsetSum(arr, sum-arr[n], n-1);
+        bool excluding = subsetSum(arr, sum, n-1);
+        return including || excluding;
+    }
+    else{
+        bool excluding = subsetSum(arr, sum, n-1);
+        return excluding;
+    }
+}
+
+bool subsetSumCache(int arr[], int sum, int n, int** cache){
+    if(n < 0){
+        return false;
+    }
     if(sum < 0){
         return false;
     }
-    int including, excluding;
-    if(n-1 >= 0){
-        if(cache[n-1][sum-arr[n]] != -1){
-            including = cache[n-1][sum-arr[n]];
-        }
-        else{
-            including = subsetSumCache(arr, sum-arr[n], n-1, cache);
-            cache[n-1][sum-arr[n]] = including;
-        }
-        if(cache[n-1][sum] != -1){
-            excluding = cache[n-1][sum];
-        }
-        else{
-            excluding = subsetSumCache(arr, sum, n-1, cache);
-            cache[n-1][sum] = excluding;
-        }
+    if(sum == 0){
+        return true;
+    }
+    if(cache[n][sum] != -1){
+        return cache[n][sum];
+    }
+    if(arr[n] <= sum){
+        bool including = subsetSumCache(arr, sum-arr[n], n-1, cache);
+        bool excluding = subsetSumCache(arr, sum, n-1, cache);
         cache[n][sum] = including || excluding;
         return cache[n][sum];
     }
     else{
-        if(sum-arr[n] == 0){
-            cache[n][sum] = 1;
-            return cache[n][sum];
-        }
-        else{
-            cache[n][sum] = 0;
-            return cache[n][sum];
-        }
+        bool excluding = subsetSumCache(arr, sum, n-1, cache);
+        cache[n][sum] = excluding;
+        return cache[n][sum];
     }
 }
 
@@ -46,19 +52,16 @@ int main(){
     int arr[] = {6, 3, 2, 1};
     int sum = 5;
     int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<subsetSum(arr, sum, n-1)<<endl;
     int** cache = new int*[n+1];
-    for(int i=0;i<n+1;i++){
+    for(int i=0;i<=n;i++){
         cache[i] = new int[sum+1];
     }
-    for(int i=0;i<n+1;i++){
-        for(int j=0;j<sum+1;j++){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=sum;j++){
             cache[i][j] = -1;
         }
     }
-    cout<<subsetSumCache(arr, sum, n-1, cache);
-    for(int i=0;i<n+1;i++){
-        delete[] cache[i];
-    }
-    delete [] cache;
+    cout<<subsetSumCache(arr,sum, n-1, cache);
     return 0;
 }
