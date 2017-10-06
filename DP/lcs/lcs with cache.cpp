07871378bd61e92731d7dc92b797ad465ebb3726
Gautam Jain <jin09@users.noreply.github.com>
2017-10-06 23:01:23 +0530

    Update lcs with cache.cpp

--- DP/lcs/lcs with cache.cpp
+++ DP/lcs/lcs with cache.cpp
@@ -5,41 +5,33 @@ int lcs(char* arr, char* brr, int first, int second, int** cache){
     if(arr[first] == '\0' || brr[second] == '\0'){
         return 0;
     }
+    if(cache[first][second] != -1){
+        return cache[first][second];
+    }
     if(arr[first] == brr[second]){
-        if(cache[first+1][second+1] != -1){
-            cout<<"cached"<<endl;
-            return 1 + cache[first+1][second+1];
-        }
-        else{
-            cout<<"no cached"<<endl;
-            int temp = lcs(arr, brr, first+1, second+1, cache);
-            cache[first+1][second+1] = temp;
-            return 1 + cache[first+1][second+1];
-        }
+        cache[first][second] = 1 + lcs(arr, brr, first+1, second+1, cache);
+        return cache[first][second];
     }
-
+    int one,two;
+    if(cache[first][second+1] != -1){
+        //cout<<"cached"<<endl;
+        one = cache[first][second+1];
+    }
     else{
-        int one,two;
-        if(cache[first][second+1] != -1){
-            cout<<"cached"<<endl;
-            one = cache[first][second+1];
-        }
-        else{
-            cout<<"no cached"<<endl;
-            one = lcs(arr, brr, first, second+1, cache);
-            cache[first][second+1] = one;
-        }
-        if(cache[first+1][second] != -1){
-            cout<<"cached"<<endl;
-            two = cache[first+1][second];
-        }
-        else{
-            cout<<"no cached"<<endl;
-            two = lcs(arr, brr, first+1, second, cache);
-            cache[first+1][second] = two;
-        }
-        return max(one, two);
+        //cout<<"no cached"<<endl;
+        one = lcs(arr, brr, first, second+1, cache);
+        cache[first][second+1] = one;
     }
+    if(cache[first+1][second] != -1){
+        //cout<<"cached"<<endl;
+        two = cache[first+1][second];
+    }
+    else{
+        //cout<<"no cached"<<endl;
+        two = lcs(arr, brr, first+1, second, cache);
+        cache[first+1][second] = two;
+    }
+    return max(one, two);
 }
 
 
@@ -48,20 +40,23 @@ int main(){
     char Y[] = "GXTXAYB";
     int l1 = strlen(X);
     int l2 = strlen(Y);
+
+    //allocate cache 2d array
     int ** arr = new int*[l1+1];
     for(int i=0;i<l1+1;i++){
         arr[i] = new int[l2+1];
     }
+
+    //set 2d array to -1
     for(int i=0;i<l1+1;i++){
         for(int j=0;j<l2+1;j++){
             arr[i][j] = -1;
         }
     }
 
-    cout<<"length of LCS is : "<<lcs(X, Y, 0, 0, arr);
+    cout<<"length of LCS is : "<<lcs(X, Y, 0, 0, arr)<<endl;
 
     //free the allocated space
-
     for(int i=0;i<l1+1;i++){
         delete[] arr[i];
     }
